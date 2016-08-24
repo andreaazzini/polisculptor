32a24ee9e17285aaf19526c144beb662bb3df303
Andrea Azzini <and.azzini@gmail.com>
2016-08-24 17:49:13 +0200

    Prepare JSON for polidata

--- decorator.js
+++ decorator.js
@@ -5,12 +5,14 @@ var fs = require('fs');
 var filename = 'data/' + process.argv[2] + '.json';
 var json = JSON.parse(fs.readFileSync(filename, 'utf8'));
 
-newJson = [];
+newJson = {};
+newJson.name = process.argv[2];
 for (obj in json) {
   json[obj].name = json[obj]._id;
   json[obj].size = json[obj].value;
   delete json[obj]._id;
   delete json[obj].value;
 }
+newJson.children = json;
 
-fs.writeFileSync(filename, JSON.stringify(json), 'utf8');
+fs.writeFileSync(filename, JSON.stringify(newJson), 'utf8');
